eb9271c28b0780ff209cfb9988b7796e0d887cf5
Dayna Clare <daycanimate@gmail.com>
2024-03-06 14:03:19 -0600

    Completed exercises 3 & 4

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,7 @@ from wtforms.validators import InputRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+    
+class UploadForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import UserProfile
-from app.forms import LoginForm
+from app.forms import LoginForm, UploadForm
 
 
 ###
@@ -26,7 +27,8 @@ def about():
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     # Instantiate your form class
-
+    form = UploadForm()
+    
     # Validate file upload on submit
     if form.validate_on_submit():
         # Get file data and save to your uploads folder
@@ -43,20 +45,32 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            # Get the username and password values from the form.
+            uname = form.username.data
+            passwd = form.password.data            
 
-        # Using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+            # Using your model, query database for a user based on the username
+            # and password submitted. Remember you need to compare the password hash.
+            # You will need to import the appropriate function to do so.
+            # Then store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method below.
+            try:
+                user = db.session.execute(db.select(UserProfile).filter_by(username=uname)).scalar_one()
+            
+                if check_password_hash(user.password, passwd) == True:            
+                    # Gets user id, load into session
+                    login_user(user)
+                    
+                    # Remember to flash a message to the user
+                    flash('Successfully Logged In!', 'success')  
+                    return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
+            except:
+                flash('Incorrect Username or Password. Please try again.', 'error')   
+           
+        flash_errors(form)
 
-        # Gets user id, load into session
-        login_user(user)
-
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
